aa82365936915826fe572bc4bbdd5ac653f2c35c
Ho-Van-Con <47619692+Ho-Van-Con@users.noreply.github.com>
2020-05-25 17:33:48 +0700

    done CLA Adder 24 bits

---
+++ ADD_CLA_24_tb.v
@@ -0,0 +1,27 @@
+module ADD_CLA_24_tb();
+
+reg   [23:0]   iA, iB;
+reg            iC;
+
+wire  [23:0]   oS;
+wire           oG, oP, oC;
+
+ADD_CLA_24       ADD_CLA_24_(.iA(iA), .iB(iB), .iC(iC), .oS(oS), .oG(oG), .oP(oP), .oC(oC));
+
+initial 
+begin
+  #0;
+  iA = 24'd125;
+  iB = 24'd11;
+  iC = 1'b0;
+  #10;
+  iA = 24'd127;
+  iB = 24'd105;
+  iC = 1'b1;
+  #10;
+  iA = 24'b11111111;
+  iB = 24'b11111111;
+  iC = 1'b1;
+end
+
+endmodule
