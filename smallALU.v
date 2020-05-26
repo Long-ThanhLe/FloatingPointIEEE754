00f4ee4e73e59dac19165bd0d9c6c7c04604fefd
Ho-Van-Con <47619692+Ho-Van-Con@users.noreply.github.com>
2020-05-26 14:04:49 +0700

    done Add/Sub cla method

--- smallALU.v
+++ smallALU.v
@@ -38,8 +38,8 @@ output sign_out;
 wire [7: 0] out_12, out_21;
 wire cout_12, cout_21;
 
-SUB_CLA_8    SUB_CLA_EXP_12(.a(in_0), .b(in_1), .cin(1'b0), .out(out_12), .cout(cout_12));
-SUB_cLA_8    SUB_CLA_EXP_21(.a(in_1), .b(in_0), .cin(1'b0), .out(out_21), .cout(cout_21));
+SUB_CLA_8    SUB_CLA_EXP_12(.iA(in_0), .iB(in_1), .iC(1'b0), .oS(out_12), .oC(cout_12), .oP(), .oG());
+SUB_CLA_8    SUB_CLA_EXP_21(.iA(in_1), .iB(in_0), .iC(1'b0), .oS(out_21), .oC(cout_21), .oP(), .oG());
 
 assign out = ({8{!cout_12}} & (out_12)) | ({8{cout_12}} & (out_21)); // differnence e1-e2
 assign sign_out = cout_12;
